9b3a346b007a1f0e8e145ed69d198c43e7b41dd7
anderjs <andersonav37@gmail.com>
2021-03-05 19:22:16 -0500

    hook: useMediaDevices

---
+++ src/hooks/useMediaDevices/index.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+
+interface MediaDevicesHook {
+  localMediaStream: MediaStream | null
+  error: Error | null
+}
+
+
+function useMediaDevices(constraints?: MediaStreamConstraints): MediaDevicesHook {
+  const [localMediaStream, setLocalMediaStream] = useState<MediaStream | null>(
+    null
+  )
+
+  const [error, setError] = useState<Error | null>(null)
+
+  useEffect(() => {
+    if (navigator?.mediaDevices?.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(setLocalMediaStream)
+        .catch(setError)
+    } else {
+      const err = new Error(
+        'Unsupoorted mediaDevices see: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices'
+      )
+
+      setError(err)
+    }
+  }, [])
+
+  return {
+    localMediaStream,
+    error
+  }
+}
+
+export default useMediaDevices
