cbc01b76cdf5cb7f9d91edaebdc846b141134f7c
anderjs <andersonav37@gmail.com>
2021-03-07 15:36:17 -0500

    <App /> Example Update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useRef } from 'react'
 
-import { useConnectionHandler } from 'react-webrtc-stream'
+import { useConnectionHandler, useMediaStream } from 'react-webrtc-stream'
 
 const App = () => {
+  const { localMediaStream, remoteMediaStream } = useMediaStream()
+
   const { connect } = useConnectionHandler()
 
   const audioRef = useRef<HTMLAudioElement>(null)
@@ -17,6 +19,17 @@ const App = () => {
 
     connectHandler()
   }, [connect])
+
+  React.useEffect(() => {
+    if (audioRef.current && videoRef.current) {
+      audioRef.current.srcObject = localMediaStream
+      videoRef.current.srcObject = localMediaStream
+    } 
+  }, [localMediaStream])
+
+  React.useEffect(() => {
+    console.log(remoteMediaStream)
+  }, [remoteMediaStream])
 
   return (
     <>
