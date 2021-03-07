73c7a30eb342c7561e740af9fc2f2e7fd8b402e7
anderjs <andersonav37@gmail.com>
2021-03-07 00:40:47 -0500

    Context

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -2,16 +2,24 @@
 import { createContext } from 'react'
 
 export interface WebRTCAdapterContext {
+  dataChannel: RTCDataChannel
+  setDataChannel: (channel: RTCDataChannel) => void
   remotePeerConnection: RTCPeerConnection
   localPeerConnection: RTCPeerConnection
+  localConnection: RTCPeerConnection
+  remoteConnection: RTCPeerConnection
   getUserMedia?: (constraints: MediaStreamConstraints) => Promise<MediaStream | null>
   setUserMedia?: (mediaStream: MediaStream) => Promise<void>
   connect: (media: MediaStreamConstraints) => Promise<void>
 }
 
 const RTC = createContext<WebRTCAdapterContext>({
+  dataChannel: new RTCDataChannel(),
+  setDataChannel: function () {},
   remotePeerConnection: new RTCPeerConnection(),
   localPeerConnection: new RTCPeerConnection(),
+  localConnection: new RTCPeerConnection(),
+  remoteConnection: new RTCPeerConnection(),
   connect: async function () {}
 })
 
