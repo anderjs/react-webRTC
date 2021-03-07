35c25fdfbb357800377975969ae153ea244ab291
anderjs <andersonav37@gmail.com>
2021-03-06 22:34:17 -0500

    <WebRTCAdapter /> Optimization

--- src/components/WebRTCAdapter/index.tsx
+++ src/components/WebRTCAdapter/index.tsx
@@ -4,6 +4,7 @@ import React, { memo, useState, useMemo } from 'react'
 import Adapter from 'components/Adapter'
 
 import RTC from 'context'
+import MediaRTC from 'context/Media'
 
 import { logTrace } from 'utils'
 
@@ -16,8 +17,9 @@ interface Props {
 interface WebRTCAdapterContext {
   remotePeerConnection: RTCPeerConnection
   localPeerConnection: RTCPeerConnection
-  localMediaStream?: MediaStream | null
-  getUserMedia?: (constraints: MediaStreamConstraints) => Promise<MediaStream | null>
+  getUserMedia?: (
+    constraints: MediaStreamConstraints
+  ) => Promise<MediaStream | null>
   setUserMedia?: (mediaStream: MediaStream) => Promise<void>
   connect: (media: MediaStreamConstraints) => Promise<void>
 }
@@ -39,6 +41,8 @@ const WebRTCAdapter: React.FunctionComponent<Props> = ({
     null
   )
 
+  const [remoteMediaStream, setRemoteMediaStream] = useState<readonly MediaStream []>([])
+
   async function getUserMedia(constraints: MediaStreamConstraints) {
     if (navigator?.mediaDevices?.getUserMedia) {
       const stream = await navigator.mediaDevices.getUserMedia(constraints)
@@ -54,35 +58,137 @@ const WebRTCAdapter: React.FunctionComponent<Props> = ({
     setLocalMediaStream(mediaStream)
   }
 
+  /**
+   * @description
+   * The `connect` method provides a direct call with the browser to request the video or audio devices.
+   * It also establishes the ICECandidate, and RTCPeerConnection protocols
+   * to achieve a connection between both browsers.
+   */
   async function connect(constraints: MediaStreamConstraints) {
     try {
       const localMedia = await getUserMedia(constraints)
 
-      const audioTracks = localMedia.getAudioTracks()[0]
+      const audioTracks = localMedia.getAudioTracks()
 
-      const videoTracks = localMedia.getVideoTracks()[0]
+      const videoTracks = localMedia.getVideoTracks()
 
-      logTrace(`Connected audio device: ${audioTracks.label}`)
-      logTrace(`Connected video device: ${videoTracks.label}`)
+      if (audioTracks.length > 0) {
+        logTrace(`Connected audio device: ${audioTracks[0].label}`)
+      }
+
+      if (videoTracks.length > 0) {
+        logTrace(`Connected video device: ${videoTracks[0].label}`)
+      }
+
+      /**
+       * @description
+       * Local Peer Connection Events
+       */
+      localPeerConnection.addEventListener('icecandidate', handleRTCIceConnection)
+
+      localPeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange)
+
+      /**
+       * @description
+       * Remote Peer Connection Events
+       */
+      remotePeerConnection.addEventListener('icecandidate', handleRTCIceConnection)
+
+      remotePeerConnection.addEventListener('iceconnectionstatechange', handleConnectionChange)
+
+      remotePeerConnection.addEventListener('track', handleRemoteStream)
     } catch (err) {
       logTrace(err.name)
     }
+  }
+
+  /**
+   * The WebRTC API interface `RTCTrackEvent` represents the `track` event, 
+   * which is sent when a new `MediaStreamTrack` is added to an `RTCRtpReceiver` which is part of the `RTCPeerConnection`. 
+   * The target is the `RTCPeerConnection` object to which the track is being added.
+   */
+  const handleRemoteStream = (event: RTCTrackEvent) => {    
+    setRemoteMediaStream(event.streams)
+
+    logTrace(`Remote stream received: ${event.track}`)
+
+  }
+
+  const handleConnectionChange = (event: Event) => {
+    const peer = event.target
+
+    logTrace(`${getPeerName(peer)} ICE State: ${peer}`)
+  }
+
+  /**
+   * @description
+   * When a web site or app using `RTCPeerConnection` receives a new ICE candidate from the remote peer
+   * over its signaling channel, it delivers the newly-received candidate to the browser's `ICE`
+   * agent by calling
+   * `RTCPeerConnection.addIceCandidate()`. This adds this new remote candidate to the
+   * `RTCPeerConnection`'s remote description, which describes the state of the remote end of the connection.
+   */
+  const handleRTCIceConnection = async (event: RTCPeerConnectionIceEvent) => {
+    const peerConnection = event.target
+
+    const iceCandidate = event.candidate
+
+    if (iceCandidate) {
+      const newIceCandidate = new RTCIceCandidate(iceCandidate)
+
+      const outgoingPeerConnection = getOutgoingPeerConnection(peerConnection)
+
+      try {
+        await outgoingPeerConnection.addIceCandidate(newIceCandidate)
+        logTrace(
+          `${getPeerName(peerConnection)} addIceCandidate: \n ${
+            event.candidate?.candidate
+          }`
+        )
+      } catch (err) {
+        logTrace(
+          `${getPeerName(
+            peerConnection
+          )} failed to add ICE Candidate: \n ${JSON.stringify(err, null, 2)}`
+        )
+      }
+    }
+  }
+
+  const getOutgoingPeerConnection = (peerConnection: EventTarget | null) => {
+    return peerConnection === localPeerConnection
+      ? remotePeerConnection
+      : localPeerConnection
+  }
+
+  const getPeerName = (peerConnection: EventTarget | null) => {
+    return peerConnection === localPeerConnection
+      ? 'remotePeerConnection'
+      : 'localPeerConnection'
   }
 
   const memo = useMemo(() => {
     return {
       localPeerConnection,
       remotePeerConnection,
-      localMediaStream,
       getUserMedia,
       setUserMedia,
       connect
-    } as WebRTCAdapterContext 
+    } as WebRTCAdapterContext
   }, [])
+
+  const mediaStreamConsumer = useMemo(() => ({
+    localMediaStream,
+    remoteMediaStream
+  }), [])
 
   return (
     <Adapter>
-      <RTC.Provider value={memo}>{children}</RTC.Provider>
+      <RTC.Provider value={memo}>
+        <MediaRTC.Provider value={mediaStreamConsumer}>
+          {children}
+        </MediaRTC.Provider>
+      </RTC.Provider>
     </Adapter>
   )
 }
