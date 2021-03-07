5c68bd9dd342b60491b0986cf888a8c29ab31a6f
anderjs <andersonav37@gmail.com>
2021-03-06 22:35:00 -0500

    Library Index Updated

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import useConnectionHandler from './hooks/useConnectionHandler'
 import useLocalPeerConnection from './hooks/useLocalPeerConnection'
 import useRemotePeerConnection from './hooks/useRemotePeerConnection'
 import useMediaDevices from './hooks/useMediaDevices'
-import useRTCIceCandidate from './hooks/useRTCIceCandidate'
+import useMediaStream from './hooks/useMediaStream'
 import useLocalMedia from './hooks/useLocalMedia'
 
 import WebRTCAdapter from './components/WebRTCAdapter'
@@ -15,7 +15,7 @@ export {
   useLocalMedia,
   useLocalPeerConnection,
   useMediaDevices,
+  useMediaStream,
   useRemotePeerConnection,
-  useRTCIceCandidate,
   WebRTCAdapter
 }
