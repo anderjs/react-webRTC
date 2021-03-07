0607e4f38b575dc2183fa3e95245aef32e560bd3
anderjs <andersonav37@gmail.com>
2021-03-07 15:36:55 -0500

    <useConnectionHandler />

--- src/hooks/useConnectionHandler/index.ts
+++ src/hooks/useConnectionHandler/index.ts
@@ -3,11 +3,22 @@ import useWebRTCAdapterState from 'hooks/useWebRTCAdapterState'
 
 import { logTrace } from 'utils'
 
+function useConnectionHandler() {
+  const {
+    connect,
+    localConnection,
+    remoteConnection,
+    setDataChannel
+  } = useWebRTCAdapterState()
 
-function useConnectionHandler () {
-  const { connect, localConnection, remoteConnection, setDataChannel } = useWebRTCAdapterState()
-
-  const createChannelContext = async () => {
+  /**
+   * @description
+   * The `createDataChannel()` method on the `RTCPeerConnection` interface creates a new channel
+   * linked with the remote peer, over which any kind of data may be transmitted.
+   * This can be useful for back-channel content such as images, file transfer,
+   * text chat, game update packets, and so forth.
+   */
+  const createDataChannel = async () => {
     logTrace('Using STCP Data Channel')
 
     const channel = localConnection.createDataChannel('DataChannel')
@@ -15,28 +26,40 @@ function useConnectionHandler () {
     setDataChannel(channel)
 
     localConnection.onicecandidate = handleLocalConnectionCallback
+
     remoteConnection.onicecandidate = handleRemoteConnectionCallback
-  } 
-  
-  const handleLocalConnectionCallback = async (event: RTCPeerConnectionIceEvent) => {
+  }
+
+  const handleLocalConnectionCallback = async (
+    event: RTCPeerConnectionIceEvent
+  ) => {
     const iceCandidate = event.candidate
 
     if (iceCandidate) {
-      await remoteConnection.addIceCandidate(iceCandidate)
+      try {
+        await remoteConnection.addIceCandidate(iceCandidate)
+      } catch (err) {
+        logTrace('Fail to attemp remoteConnection iceCandidate')
+      }
     }
   }
 
-  const handleRemoteConnectionCallback = async (event: RTCPeerConnectionIceEvent) =>  {
+  const handleRemoteConnectionCallback = async (
+    event: RTCPeerConnectionIceEvent
+  ) => {
     const iceCandidate = event.candidate
 
     if (iceCandidate) {
-      await localConnection.addIceCandidate(iceCandidate)
+      try {
+        await localConnection.addIceCandidate(iceCandidate)
+      } catch (err) {
+        logTrace('Fail to attemp localConnection iceCandidate')
+      }
     }
   }
-
 
   return {
-    createChannelContext,
+    createDataChannel,
     connect
   }
 }
