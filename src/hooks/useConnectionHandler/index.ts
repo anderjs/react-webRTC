3fe360940a5c914f4fc2414af72760f9abcced87
anderjs <andersonav37@gmail.com>
2021-03-07 00:40:29 -0500

    createChannelContext

--- src/hooks/useConnectionHandler/index.ts
+++ src/hooks/useConnectionHandler/index.ts
@@ -1,10 +1,23 @@
 /* eslint-disable prettier/prettier */
 import useWebRTCAdapterState from 'hooks/useWebRTCAdapterState'
 
+import { logTrace } from 'utils'
+
+
 function useConnectionHandler () {
-  const { connect } = useWebRTCAdapterState()
+  const { connect, localConnection, setDataChannel } = useWebRTCAdapterState()
+
+  const createChannelContext = () => {
+    logTrace('Using STCP Data Channel')
+
+    const channel = localConnection.createDataChannel('DataChannel')
+
+    setDataChannel(channel)
+  } 
+
 
   return {
+    createChannelContext,
     connect
   }
 }
